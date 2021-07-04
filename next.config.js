168eccef7cb5af168fbfb5a64b96a0a87ffe8634
chutchai.kp <chutchai.kp@gmail.com>
2021-07-04 10:06:56 +0700

    Do ignore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    REACT_APP_GRAPHQL_SCHEMA: 'https://xxx.herokuapp.com/graphql',
-    REACT_APP_API_URL: 'https://xxx.herokuapp.com/todos?_sort=updatedAt:DESC'
+    REACT_APP_GRAPHQL_SCHEMA: 'https://strpi-atlas.herokuapp.com/graphql',
+    REACT_APP_API_URL: 'https://strpi-atlas.herokuapp.com/todos?_sort=updatedAt:DESC'
   },
   reactStrictMode: true,
 }
