b3a653fa8db87636fb74879908bb772289145381
chutchai.kp <chutchai.kp@gmail.com>
2021-07-04 08:28:19 +0700

    Init commit

--- pages/MyApolloClient.tsx
+++ pages/MyApolloClient.tsx
@@ -1,23 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { ApolloClient, ApolloProvider, createHttpLink, DefaultOptions, InMemoryCache } from '@apollo/client';
-
-// require('dotenv').config()
-
-import theme from './themes/theme'
+import { ApolloClient, createHttpLink, DefaultOptions, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-// import { client } from './MyApolloClient';
-
 
 // const uri = "https://strpi-atlas.herokuapp.com/graphql"; //  'http://localhost:1337/graphql';
 // const client = new ApolloClient({ uri, cache: new InMemoryCache() });
-
 const { REACT_APP_GRAPHQL_SCHEMA } = process.env;
-
-console.log(REACT_APP_GRAPHQL_SCHEMA)
-
 const httpLink = createHttpLink({
   uri: REACT_APP_GRAPHQL_SCHEMA,
 });
@@ -43,20 +29,8 @@ const defaultOptions: DefaultOptions = {
     errorPolicy: 'all',
   },
 };
-const client = new ApolloClient({
+export const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   defaultOptions: defaultOptions,
 });
-
-function MyApp({ Component, pageProps }: AppProps) {
-
-  return (
-    <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
-  )
-}
-export default MyApp
