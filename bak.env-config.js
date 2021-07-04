c476dbf41e0679e3739392c6e1231bf30870dec1
chutchai.kp <chutchai.kp@gmail.com>
2021-07-04 09:30:55 +0700

    OK

---
+++ bak.env-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'process.env.REACT_APP_GRAPHQL_SCHEMA': 'https://strpi-atlas.herokuapp.com/graphql',
+	'process.env.REACT_APP_API_URL': 'https://strpi-atlas.herokuapp.com/todos?_sort=updatedAt:DESC',
+	'process.env.REACT_APP_API_URL_V2': 'https://strpi-atlas.herokuapp.com/todos?_limit=2&_start=0'
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  env: {
+    REACT_APP_GRAPHQL_SCHEMA: 'https://strpi-atlas.herokuapp.com/graphql',
+    REACT_APP_API_URL: 'https://strpi-atlas.herokuapp.com/todos?_sort=updatedAt:DESC'
+  },
   reactStrictMode: true,
 }
 
